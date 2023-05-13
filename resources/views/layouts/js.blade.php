27bbdaf1a98b8a47f53541e64d67821022dbf81a
Arif Laksonodhewo <notsafeforworked@gmail.com>
2023-05-13 19:29:50 +0700

    fixing reorder link and script

--- resources/views/layouts/js.blade.php
+++ resources/views/layouts/js.blade.php
@@ -1,12 +1,12 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/fixedheader/3.3.2/js/dataTables.fixedHeader.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.4.1/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/fixedheader/3.3.2/js/dataTables.fixedHeader.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="{{ asset('js/script.js') }}"></script>
 
 {{-- <script src="{{ asset('js/jQuery@3.6.4.min.js') }}"></script> --}}
 {{-- <script src="{{ asset('js/dataTables@1.13.4.min.js') }}"></script> --}}
 {{-- <script src="{{ asset('js/dataTables.responsive@2.4.1.min.js') }}"></script> --}}
 {{-- <script src="{{ asset('js/dataTables.fixed-header@3.3.2.min.js') }}"></script>  --}}
 {{-- <script src="{{ asset('js/sweetalert2@11.min.js') }}"></script> --}}
-{{-- <script src="{{ asset('js/script.js') }}"></script> --}}
